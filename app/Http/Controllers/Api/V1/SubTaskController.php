2c0360c9b11aa74cbd0803b10f8f1201286f43c5
jdestin <destin.jord@gmail.com>
2023-03-31 17:40:35 +0200

    update subtaskcontroller

--- app/Http/Controllers/Api/V1/SubTaskController.php
+++ app/Http/Controllers/Api/V1/SubTaskController.php
@@ -4,22 +4,83 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Statutes;
+use App\Models\Theme;
+use App\Models\Task;
+use App\Models\Subtasks;
+use App\Http\Resources\SubtaskResource;
+use App\Http\Requests\StoreSubtaskRequest;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
-class StatutesController extends Controller
+class SubTaskController extends Controller
 {
     /**
-     * Handle the incoming request.
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function index(Theme $theme, Task $task)
+    {
+        $subtask = Subtasks::where('task_id', $task->id)->latest()->get();
+
+        return SubtaskResource::collection($subtask);
+    }
+
+    /**
+     * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function store(StoreSubtaskRequest $request, Theme $theme, Task $task)
     {
-        $listStatus = Statutes::all();
+        $data = $request->validated();
+        $data['task_id'] = $task->id;
 
-        return response()->json([
-            'listStatus'=>$listStatus
-        ], 200);
+        $subtask = Subtasks::create($data);
+        
+        return response()->json(200);           
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,Theme $theme,Task $task, Subtask $subtask)
+    {
+        $updateSubtask = Subtask::where("id", $subtask->id)
+                    ->update([
+                        "validate" => $request->validate
+                    ]);
+        
+        return response()->json(200);
+
+    }
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Theme $theme, Task $task, Subtask $subtask)
+    {
+        $subtask->delete();
+        return response()->noContent();
     }
 }
